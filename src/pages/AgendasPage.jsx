44ad41d31c6bd2a2bcca0ea9761be8b3c8510fbc
Joe <joediaz88@hotmail.com>
2024-12-22 15:43:58 +0000

    trasladadas funciones get y post a api js

--- src/pages/AgendasPage.jsx
+++ src/pages/AgendasPage.jsx
@@ -1,41 +1,22 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { NavLink } from "react-router";
-
+import { getAgendas, postAgendas } from "../services/api";
 
 export const Agendas = () => {
   const [agendas, setAgendas] = useState([]);
   const [isUserHovered, setIsUserHovered] = useState("");
-  const [inputValue, setInputValue] = useState("");
+  const [inputAgendaValue, setInputAgendaValue] = useState("");
+
   useEffect(() => {
-    fetchAgendas(setAgendas)
+    getAgendas(setAgendas)
   }, []);
 
-  //función para importar listado de agendas
-  const fetchAgendas = () => {
-    fetch(`https://playground.4geeks.com/contact/agendas`, {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((response) => {
-        setAgendas(response.agendas)
-      });
-  };
-
   // función para crear nueva agenda
-  const postAgenda = (e) => {
-    if (e.key === "Enter" && inputValue.trim() !== "") {
-      return fetch(`https://playground.4geeks.com/contact/agendas/${inputValue}`, {
-        method: "POST",
-        headers: { "Content-type": "application/json" }
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setAgendas([...agendas, { slug: inputValue }]);
-          setInputValue("");
-        })
-        .catch((err) => console.log(`error:`, err));
-
+  const addAgenda = (e) => {
+    if (e.key === "Enter" && inputAgendaValue.trim() !== "") {
+      postAgendas(inputAgendaValue, agendas, setAgendas);
+      setInputAgendaValue("");
     }
   }
 
@@ -45,37 +26,37 @@ export const Agendas = () => {
     }
     )
       .then(() => {
-        fetchAgendas(setAgendas)
+        getAgendas(setAgendas)
       })
   }
 
   return (
     <>
       <div className="input mb-1 text-center">
-        <input className="border rounded input" value={inputValue} type="text" placeholder="Post new agenda" onKeyDown={(e) => postAgenda(e)}
-          onChange={(e) => setInputValue(e.target.value)} />
+        <input className="border rounded input" value={inputAgendaValue} type="text" placeholder="Post new agenda" onKeyDown={(e) => addAgenda(e)}
+          onChange={(e) => setInputAgendaValue(e.target.value)} />
       </div>
       <div className="cajaAgendas">
         {agendas.map((person) => {
           return (
-            
-              <div className="agendas m-1 border rounded fw-bold" key={person.slug} onMouseEnter={() => setIsUserHovered(person.slug)}
-                onMouseLeave={() => setIsUserHovered("")}>
-                <NavLink
-                  to={`/agendas/${person.slug}/contacts`} end>{person.slug}</NavLink>
-                <div className="cajaCloseBtn">
-                  <span
-                    className="closeBtn fw-bold"
-                    role="button"
-                    style={{
-                      visibility: isUserHovered === person.slug ? "visible" : "hidden",
-                    }}
-                    onClick={() => deleteAgenda(person.slug)}
-                  >
-                    X
-                  </span>
-                </div>
+
+            <div className="agendas m-1 border rounded fw-bold" key={person.slug} onMouseEnter={() => setIsUserHovered(person.slug)}
+              onMouseLeave={() => setIsUserHovered("")}>
+              <NavLink
+                to={`/agendas/${person.slug}/contacts`} end>{person.slug}</NavLink>
+              <div className="cajaCloseBtn">
+                <span
+                  className="closeBtn fw-bold"
+                  role="button"
+                  style={{
+                    visibility: isUserHovered === person.slug ? "visible" : "hidden",
+                  }}
+                  onClick={() => deleteAgenda(person.slug)}
+                >
+                  X
+                </span>
               </div>
+            </div>
           );
         })}
       </div>
